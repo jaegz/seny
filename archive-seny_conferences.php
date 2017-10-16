3e393b8e7fe6c6509338dd6295f25dc24fe43466
cjaeger <chris_jaeger@nylim.com>
2017-10-16 17:23:21 -0400

    pull conference date for conference archive page loop

--- archive-seny_conferences.php
+++ archive-seny_conferences.php
@@ -15,12 +15,12 @@ get_header(); ?>
 
 <section id="main" class="container">
 	<?php
-	$conf_args = array( 
-		'post_type' => 'seny_conferences', 
+	$conf_args = array(
+		'post_type' => 'seny_conferences',
 		'posts_per_page' => 10
 	);
 	$conf_loop = new WP_Query( $conf_args );
-		
+
 	while ( $conf_loop->have_posts() ) : $conf_loop->the_post(); ?>
 		<div class="box">
 	  		<span><?php echo get_field('conference_date'); ?></span>
